9d45d59dd7a2324bacaeabe939a79b632c992a13
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-11-21 19:25:24 -0600

    Install formik

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
 function Login() {
-  return (
-    <div className='container-sm mt-5 p-5'>
-      <h2 className='text-center'>Login</h2>
-      <div className='mb-3'>
-        <label for='exampleFormControlInput1' className='form-label'>
-          Email address
-        </label>
-        <input
-          type='email'
-          className='form-control'
-          id='exampleFormControlInput1'
-          placeholder='name@example.com'
-        />
-      </div>
-    </div>
-  );
+  return <div>Login</div>;
 }
 
 export default Login;
